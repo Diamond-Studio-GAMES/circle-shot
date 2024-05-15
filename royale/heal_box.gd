45a5eeff44fd1842f6c4c5cc513c63c84a364bec
Summersay415 <summersay415@gmail.com>
2024-05-15 16:12:00 +0000

    Ещё немного

--- royale/heal_box.gd
+++ royale/heal_box.gd
@@ -13,5 +13,7 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_despawn_timer_timeout() -> void:
+	($AnimationPlayer as AnimationPlayer).play(&"Despawn")
 	if multiplayer.is_server():
+		await ($AnimationPlayer as AnimationPlayer).animation_finished
 		queue_free()
